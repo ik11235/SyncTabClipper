d2d2e33ddcc8c981dbf62528d7a5690271e09bc4
ik-fib <ik11235@gmail.com>
2020-03-18 22:01:41 +0900

    jsonデータで出力するように

--- src/background.js
+++ src/background.js
@@ -1,8 +1,20 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
+    var json = {
+        created_at: new Date(),
+        tabs: []
+    };
+
     chrome.tabs.query({currentWindow: true}, function (tabs) {
         for (var i = 0; i < tabs.length; i++) {
-            console.log('url:' + tabs[i].url + ' ' + tabs[i].title);
+            var tab_data = {
+                url: tabs[i].url,
+                title: tabs[i].title
+            };
+            console.log(tab_data);
+            json.tabs.push(tab_data);
         }
         console.log('現在開いているタブの数は[' + tabs.length + ']個です');
+        console.log(json);
     });
+
 });
