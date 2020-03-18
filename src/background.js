e04020a6c071e828c2cf1bb7c1f1fa6301a79cdf
ik-fib <ik11235@gmail.com>
2020-03-18 23:59:05 +0900

    不要なvarをconstに 不要なコメントを削除

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 chrome.contextMenus.removeAll();
 
 //親メニュー
-var parentId = chrome.contextMenus.create({
+const parentId = chrome.contextMenus.create({
     "title": "syncTabClipper",
     "type": "normal",
     "contexts": ["all"],
@@ -14,8 +14,7 @@ chrome.contextMenus.create({
     "contexts": ["all"],
     "onclick": function (info) {
         // https://gist.github.com/syoichi/3747507
-        var id = chrome.runtime.id;
-        var url = chrome.runtime.getURL('tabs.html');
+        const url = chrome.runtime.getURL('tabs.html');
         chrome.tabs.create({
             selected: true,
             url: url
@@ -41,15 +40,12 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
     chrome.tabs.query({currentWindow: true}, function (tabs) {
         for (var i = 0; i < tabs.length; i++) {
-            var tab_data = {
+            const tab_data = {
                 url: tabs[i].url,
                 title: tabs[i].title
             };
-            console.log(tab_data);
             json.tabs.push(tab_data);
         }
-        console.log('現在開いているタブの数は[' + tabs.length + ']個です');
-        console.log(json);
         chrome.storage.sync.set({'tab_data': json}, function () {
         });
     });
--- src/tabs.js
+++ src/tabs.js
@@ -1,21 +1,19 @@
 function getDomein(str) {
-    var parser = new URL(str);
+    const parser = new URL(str);
     return parser.host;
 }
 
 window.onload = function () {
     chrome.storage.sync.get(['tab_data'], function (result) {
         //console.dir(result);
-        var main = document.getElementById('main');
-        var inner_html = '';
+        const main = document.getElementById('main');
 
-        var links = result.tab_data.tabs.map(function (page_data) {
+        const links = result.tab_data.tabs.map(function (page_data) {
             var domain = getDomein(page_data.url);
             var str = `<li><img src="http://www.google.com/s2/favicons?domain=${domain}" alt="${page_data.title}"/><a href="${page_data.url}">${page_data.title}</a></li>`;
             console.log(str);
             return str;
         }).join("\n");
-        console.log(links);
 
         const html_text = `<ul>${links}</ul>`;
         main.insertAdjacentHTML('afterbegin', html_text);
