1bcc403f5b4c2ccd5dc02a13dd472446d9736e32
ik-fib <ik11235@gmail.com>
2020-03-18 21:34:46 +0900

    urlとタイトルを出力するように

--- src/background.js
+++ src/background.js
@@ -1,5 +1,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({}, function (tabs) {
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+            console.log('url:' + tabs[i].url + ' ' + tabs[i].title);
+        }
         console.log('現在開いているタブの数は[' + tabs.length + ']個です');
     });
 });
