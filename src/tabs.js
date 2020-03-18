984d5c4ce558b0f1b551d4263fef4fdfef07afc3
ik-fib <ik11235@gmail.com>
2020-03-18 23:57:35 +0900

    雑な箇条書きフォーマットで出力するように

--- src/tabs.js
+++ src/tabs.js
@@ -1,7 +1,23 @@
+function getDomein(str) {
+    var parser = new URL(str);
+    return parser.host;
+}
+
 window.onload = function () {
     chrome.storage.sync.get(['tab_data'], function (result) {
-        console.dir(result);
+        //console.dir(result);
         var main = document.getElementById('main');
-        main.innerHTML = JSON.stringify(result);
+        var inner_html = '';
+
+        var links = result.tab_data.tabs.map(function (page_data) {
+            var domain = getDomein(page_data.url);
+            var str = `<li><img src="http://www.google.com/s2/favicons?domain=${domain}" alt="${page_data.title}"/><a href="${page_data.url}">${page_data.title}</a></li>`;
+            console.log(str);
+            return str;
+        }).join("\n");
+        console.log(links);
+
+        const html_text = `<ul>${links}</ul>`;
+        main.insertAdjacentHTML('afterbegin', html_text);
     });
 };
