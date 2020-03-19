acc912492105865979d34168a608e706943fc831
ik-fib <ik11235@gmail.com>
2020-03-20 03:12:05 +0900

    クリックしたらアイテムを消えるように

--- src/tabs.js
+++ src/tabs.js
@@ -21,6 +21,58 @@ function allClear() {
     });
 }
 
+function jsonFromHtml(dom) {
+    const created_at = dom.getAttribute("data-created-at");
+
+    var json = {
+        created_at: created_at,
+        tabs: []
+    };
+
+    const linkDoms = dom.getElementsByClassName('tab_link');
+    for (var j = 0; j < linkDoms.length; j++) {
+        const tab_data = {
+            url: linkDoms[j].getAttribute("data-url"),
+            title: linkDoms[j].getAttribute("data-title")
+        };
+        json.tabs.push(tab_data);
+    }
+
+    return json;
+}
+
+function deleteLink(target) {
+    console.dir(target);
+    const parentDiv = target.parentNode.parentNode.parentNode;
+    //console.dir(parentDiv);
+    // 先にsyncに保存済みのデータを消したいがDom→JSONがやりにくくなる
+    // いったん、DOM消しを先にする
+    const li = target.parentNode;
+    li.parentNode.removeChild(li);
+
+    const id = parentDiv.id;
+    const json = jsonFromHtml(parentDiv);
+
+    var save_obj = {};
+    save_obj[id] = json;
+    console.dir(save_obj);
+    chrome.storage.sync.set(save_obj, function () {
+        var error = chrome.runtime.lastError;
+        if (error) {
+            alert(error.message);
+        }
+    });
+}
+
+function clickLinkByEventListener() {
+    const target = this;
+    console.dir(target);
+    var url = target.getAttribute("data-url");
+    chrome.tabs.create({url: url, active: false}, function () {
+        deleteLink(target);
+    });
+}
+
 window.onload = function () {
     const all_clear = document.getElementById('all_clear');
     all_clear.addEventListener('click', allClear);
@@ -38,15 +90,21 @@ window.onload = function () {
                     const main = document.getElementById('main');
                     if (!isEmpty(result)) {
                         const tab_datas = result["tab_datas_" + cnt];
+                        const created_at = tab_datas.created_at;
                         const tabs = tab_datas.tabs.map(function (page_data) {
                             var domain = getDomein(page_data.url);
                             // target="_blank"じゃなくて、データ削除する→newtab開くの専用関数でもいいかも
-                            var str = `<li><img src="http://www.google.com/s2/favicons?domain=${domain}" alt="${page_data.title}"/><a href="${page_data.url}" target="_blank">${page_data.title}</a></li>`;
+                            var str = `<li><img src="https://www.google.com/s2/favicons?domain=${domain}" alt="${page_data.title}"/><a href="#" class="tab_link" data-url="${page_data.url}" data-title="${page_data.title}">${page_data.title}</a></li>`;
                             console.log(str);
                             return str;
                         }).join("\n");
 
-                        main.insertAdjacentHTML('afterbegin', `<div class="tabs"><ul>${tabs}</ul></div>`);
+                        main.insertAdjacentHTML('afterbegin', `<div id="tab_datas_${cnt}" class="tabs" data-created-at="${created_at}"><ul>${tabs}</ul></div>`);
+
+                        const linkDoms = main.getElementsByClassName('tab_link');
+                        for (var j = 0; j < linkDoms.length; j++) {
+                            linkDoms[j].addEventListener('click', clickLinkByEventListener);
+                        }
                     }
                 });
             }
