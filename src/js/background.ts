dc8f5159abecf491b778761ece99076444d64539
ik-fib <ik11235@gmail.com>
2021-07-25 01:13:46 +0900

    contextMenusの開き方が変わっていたので対応

--- src/js/background.ts
+++ src/js/background.ts
@@ -6,14 +6,52 @@ importScripts("js/zlib.js");
 importScripts("js/zlib-deflate.js");
 importScripts("js/zlib-inflate.js");
 
-(function () {
-    // contextMenusに関する操作
-    chrome.contextMenus.removeAll();
+chrome.runtime.onInstalled.addListener(function () {
+    const parentId = "syncTabClipper-ContextMenu01"
+    const openMenuId = "syncTabClipper-OpenNew-tab"
 
-    //親メニュー
-    const parentId = chrome.contextMenus.create({
-        "title": "syncTabClipper",
+
+    chrome.contextMenus.create({
+        id: parentId,
+        title: "syncTabClipper",
+        type: "normal",
+        contexts: ["all"],
+    });
+
+    chrome.contextMenus.create({
+        id: openMenuId,
+        title: "tabページを開く",
+        parentId: parentId,
+        type: "normal",
+        contexts: ["all"],
+    });
+})
+
+chrome.contextMenus.onClicked.addListener(function (info, tab): void {
+    // https://github.com/STRockefeller/MyProgrammingNote/blob/d985df2a7b4cbae0c3b2e4425292c377e429fcdb/My%20Notes/%E7%A8%8B%E5%BC%8F%E5%AD%B8%E7%BF%92%E7%AD%86%E8%A8%98/Web/Chrome%20Extension/Manifest%20Version%203.md#background-service-workers
+    switch (info.menuItemId) {
+        case "syncTabClipper-OpenNew-tab":
+            // https://gist.github.com/syoichi/3747507
+            const url = chrome.runtime.getURL('tabs.html');
+            console.log(url)
+            /*
+            chrome.tabs.create({
+                selected: true,
+                url: url
+            });
+             */
+            break;
+    }
+});
+
+
+/*
+chrome.runtime.onInstalled.addListener(function () {
+    const parentId = "syncTabClipper-ContextMenu01"
+    chrome.contextMenus.create({
+        "id": parentId,
         "type": "normal",
+        "title": "syncTabClipper",
         "contexts": ["all"],
     });
 
@@ -31,7 +69,7 @@ importScripts("js/zlib-inflate.js");
             });
         }
     });
-}());
+});
 
 chrome.action.onClicked.addListener(function () {
     chrome.storage.sync.get([util.getTabLengthKey()], function (result) {
@@ -70,3 +108,4 @@ chrome.action.onClicked.addListener(function () {
         });
     });
 });
+*/
