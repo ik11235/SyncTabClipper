71730fe3062cb017c1fb3e901a7c1a1e80e815f6
ik-fib <ik11235@gmail.com>
2021-07-25 18:54:10 +0900

    ChromeServiceを触る系を分割

--- src/js/background.ts
+++ src/js/background.ts
@@ -1,5 +1,5 @@
-import * as util from './util';
 import {blockService} from "./blockService";
+import {chromeService} from "./chromeService";
 
 (function () {
     // contextMenusに関する操作
@@ -29,12 +29,12 @@ import {blockService} from "./blockService";
 }());
 
 chrome.browserAction.onClicked.addListener(function () {
-    chrome.storage.sync.get([util.getTabLengthKey()], function (result) {
-        const tab_length = util.getTabLengthOrZero(result);
+    chrome.storage.sync.get([chromeService.storage.getTabLengthKey()], function (result) {
+        const tab_length = chromeService.storage.getTabLengthOrZero(result);
         chrome.tabs.query({currentWindow: true}, function (tabs: chrome.tabs.Tab[]) {
             const block = blockService.createBlock(tabs, new Date());
 
-            const key_str = util.getTabKey(tab_length);
+            const key_str = chromeService.storage.getTabKey(tab_length);
             let save_obj: { [key: string]: string; } = {};
             save_obj[key_str] = blockService.deflateBlock(block)
             chrome.storage.sync.set(save_obj, function () {
@@ -43,7 +43,7 @@ chrome.browserAction.onClicked.addListener(function () {
                     alert(error.message);
                 } else {
                     let set_data: { [key: string]: number; } = {};
-                    set_data[util.getTabLengthKey()] = tab_length + 1;
+                    set_data[chromeService.storage.getTabLengthKey()] = tab_length + 1;
                     chrome.storage.sync.set(set_data, function () {
                         const error = chrome.runtime.lastError;
                         if (error) {
