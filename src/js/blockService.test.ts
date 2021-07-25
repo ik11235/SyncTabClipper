cf08069ecf7b86fff52bede33c47d80cb6266ed3
ik-fib <ik11235@gmail.com>
2021-07-25 18:07:24 +0900

    mockを毎回リセットする
    - https://qiita.com/s_karuta/items/ee211251d944e72b2517
    - https://qiita.com/yamagen0915/items/da885b3fa5cb825ccca9

--- src/js/blockService.test.ts
+++ src/js/blockService.test.ts
@@ -4,6 +4,19 @@
 import {blockService} from "./blockService";
 import {zlibWrapper} from "./zlib-wrapper";
 
+let deflateSpy: jest.SpyInstance;
+let inflateSpy: jest.SpyInstance;
+
+beforeAll(() => {
+    deflateSpy = jest.spyOn(zlibWrapper, 'deflate')
+    inflateSpy = jest.spyOn(zlibWrapper, 'inflate')
+});
+
+afterEach(() => {
+    deflateSpy.mockReset()
+    inflateSpy.mockReset()
+});
+
 describe('blockService', (): void => {
     test('createBlock 正常系', (): void => {
         const tabs: chrome.tabs.Tab[] = [
@@ -288,7 +301,7 @@ describe('blockService', (): void => {
     })
 
     test('deflateBlock 非圧縮時', (): void => {
-        const zlibSpy = jest.spyOn(zlibWrapper, 'deflate').mockReturnValueOnce('eNpSNXdSNTJKLkpNLElNiU8sAXJUjR0NzQwsTU3NzExMzcwtVI2cgaIliUnFEElVUydVsK7SohyoiJFRRklJQTGY6QZEqRWJuQU5qXrJ+blAXklqMdhciDmZJTmpcG1gni5MgbmLqqkLkAQAAAD//w==');
+        deflateSpy.mockReturnValueOnce('eNpSNXdSNTJKLkpNLElNiU8sAXJUjR0NzQwsTU3NzExMzcwtVI2cgaIliUnFEElVUydVsK7SohyoiJFRRklJQTGY6QZEqRWJuQU5qXrJ+blAXklqMdhciDmZJTmpcG1gni5MgbmLqqkLkAQAAAD//w==');
 
         const block = {
             created_at: new Date(`2021-01-02T03:04:05.678Z`),
@@ -301,12 +314,12 @@ describe('blockService', (): void => {
         }
         const expected = "{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"}]}"
         expect(blockService.deflateBlock(block)).toBe(expected)
-        expect(zlibSpy.mock.calls[0]).toEqual(["{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"}]}"])
-        expect(zlibSpy).toHaveBeenCalledTimes(1)
+        expect(deflateSpy.mock.calls[0]).toEqual(["{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"}]}"])
+        expect(deflateSpy).toHaveBeenCalledTimes(1)
     })
 
     test('deflateBlock 圧縮時', (): void => {
-        const zlibSpy = jest.spyOn(zlibWrapper, 'deflate').mockReturnValueOnce('eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w==');
+        deflateSpy.mockReturnValueOnce('eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w==');
 
         const block = {
             created_at: new Date(`2021-01-02T03:04:05.678Z`),
@@ -331,12 +344,12 @@ describe('blockService', (): void => {
         }
         const expected = "eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w=="
         expect(blockService.deflateBlock(block)).toBe(expected)
-        expect(zlibSpy.mock.calls[0]).toEqual(["{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"},{\"url\":\"http://google.com/test2\",\"title\":\"google-test\"},{\"url\":\"http://google.com/test3\",\"title\":\"google-test33\"},{\"url\":\"http://google.com/test4\",\"title\":\"google-test44\"}]}"])
-        expect(zlibSpy).toHaveBeenCalledTimes(1)
+        expect(deflateSpy.mock.calls[0]).toEqual(["{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"},{\"url\":\"http://google.com/test2\",\"title\":\"google-test\"},{\"url\":\"http://google.com/test3\",\"title\":\"google-test33\"},{\"url\":\"http://google.com/test4\",\"title\":\"google-test44\"}]}"])
+        expect(deflateSpy).toHaveBeenCalledTimes(1)
     })
 
     test('inflateJson 非解凍時', (): void => {
-        const zlibSpy = jest.spyOn(zlibWrapper, 'inflate').mockReturnValueOnce("{\"created_at\":1627200615501,\"tabs\":[{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome://extensions/\",\"title\":\"拡張機能\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome://newtab/\",\"title\":\"新しいタブ\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"}]}");
+        inflateSpy.mockReturnValueOnce("{\"created_at\":1627200615501,\"tabs\":[{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome://extensions/\",\"title\":\"拡張機能\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"},{\"url\":\"chrome://newtab/\",\"title\":\"新しいタブ\"},{\"url\":\"chrome-extension://djamgplmdfdnghbcpfgpbfadipbgihbi/tabs.html\",\"title\":\"syncTabCliper\"}]}");
 
         const input = "{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"},{\"url\":\"http://google.com/test2\",\"title\":\"google-test\"}]}"
 
@@ -354,11 +367,11 @@ describe('blockService', (): void => {
             ],
         }
         expect(blockService.inflateJson(input)).toStrictEqual(expected)
-        expect(zlibSpy).toBeCalledTimes(0)
+        expect(inflateSpy).toBeCalledTimes(0)
     })
 
     test('inflateJson 解凍', (): void => {
-        const zlibSpy = jest.spyOn(zlibWrapper, 'inflate').mockReturnValueOnce("{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"},{\"url\":\"http://google.com/test2\",\"title\":\"google-test\"},{\"url\":\"http://google.com/test3\",\"title\":\"google-test33\"},{\"url\":\"http://google.com/test4\",\"title\":\"google-test44\"}]}");
+        inflateSpy.mockReturnValueOnce("{\"created_at\":1609556645678,\"tabs\":[{\"url\":\"https://example.com/test\",\"title\":\"title-test\"},{\"url\":\"http://google.com/test2\",\"title\":\"google-test\"},{\"url\":\"http://google.com/test3\",\"title\":\"google-test33\"},{\"url\":\"http://google.com/test4\",\"title\":\"google-test44\"}]}");
 
         const input = 'eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w=='
 
@@ -384,9 +397,9 @@ describe('blockService', (): void => {
             ],
         }
         expect(blockService.inflateJson(input)).toStrictEqual(expected)
-        expect(zlibSpy.mock.calls[0]).toEqual(['eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w=='])
+        expect(inflateSpy.mock.calls[0]).toEqual(['eNqkzNEKwjAMBdCvyaOiadPqo3P4G1JnmEJHxxrBz7e2cyAoA4VSetObA7YCxGZgJ3w+OkkB1G5tVlsiYzQZuwHcp6m4UyyfQBXkrdvgxwniRaSP+XlIh++u6z0vm9ClJByzW5yreJ7Wclq8CrZ+dj7aE92G0L7J+IUuxf9sNW8r9bOu53WtR53qdD8AAAD//w=='])
 
-        expect(zlibSpy).toBeCalledTimes(1)
+        expect(inflateSpy).toBeCalledTimes(1)
     })
 
 
