fbe8a23ea50302426fba77586df4f53bbe6eaa3b
ik-fib <ik11235@gmail.com>
2021-07-25 18:56:08 +0900

    既存テストの移動

--- src/js/util.test.ts
+++ src/js/util.test.ts
@@ -9,20 +9,4 @@ describe('util', (): void => {
         const res = util.getDomain("test@example.com")
         expect(res).toBe('');
     });
-
-    test('getTabLengthOrZero null->0', (): void => {
-        const res = util.getTabLengthOrZero(null)
-        const expected = 0
-        expect(res).toStrictEqual(expected);
-    });
-    test('getTabLengthOrZero interger->number', (): void => {
-        const res = util.getTabLengthOrZero(123)
-        const expected = 123
-        expect(res).toStrictEqual(expected);
-    });
-    test('getTabLengthOrZero result->number', (): void => {
-        const res = util.getTabLengthOrZero({"t_len": 456})
-        const expected = 456
-        expect(res).toStrictEqual(expected);
-    });
 })
