ee883b156bc4728ae5349b9d05baeeceac72af5b
ik-fib <ik11235@gmail.com>
2021-08-07 17:39:03 +0900

    Block側からTab削除メソッドを渡して描画変更するように修正

--- src/js/components/block.tsx
+++ src/js/components/block.tsx
@@ -29,67 +29,75 @@ export const MainDom = (blocks: model.BlockAndKey[]) => <Main BlockAndKeys={bloc
 
 
 const Block: React.FC<model.BlockAndKey> = (block) => {
-    const created_at = block.block.created_at;
+    const [nowBlock, setNowBlock] = useState(block.block);
+    const created_at = nowBlock.created_at;
+    const deleteClick = (index: number) => {
+        setNowBlock((block) => {
+            block.tabs.splice(index, 1)
+            return {
+                created_at: block.created_at,
+                tabs: block.tabs
+            }
+        });
+    }
 
-    return (
-        <div id={block.IDkey} className="tabs uk-card-default block-root-dom" data-created-at="${created_at.getTime()}">
-            <div className="uk-card-header">
-                <h3 className="uk-card-title uk-margin-remove-bottom">{block.block.tabs.length}個のタブ</h3>
-                <p className="uk-text-meta uk-margin-remove-top">作成日: <time
-                    dateTime={created_at.toISOString()}>${created_at.toISOString()}</time></p>
-                <div className="uk-grid">
-                    <div className="uk-width-auto"><span className="all_tab_link uk-link">すべてのリンクを開く</span></div>
-                    <div className="uk-width-auto"><span className="all_tab_delete uk-link">すべてのリンクを閉じる</span></div>
-                    <div className="uk-width-expand"></div>
+    if (nowBlock.tabs.length > 0) {
+        return (
+            <div id={block.IDkey} className="tabs uk-card-default block-root-dom"
+                 data-created-at="${created_at.getTime()}">
+                <div className="uk-card-header">
+                    <h3 className="uk-card-title uk-margin-remove-bottom">{nowBlock.tabs.length}個のタブ</h3>
+                    <p className="uk-text-meta uk-margin-remove-top">作成日: <time
+                        dateTime={created_at.toISOString()}>{created_at.toISOString()}</time></p>
+                    <div className="uk-grid">
+                        <div className="uk-width-auto"><span className="all_tab_link uk-link">すべてのリンクを開く</span></div>
+                        <div className="uk-width-auto"><span className="all_tab_delete uk-link">すべてのリンクを閉じる</span></div>
+                        <div className="uk-width-expand"></div>
+                    </div>
+                </div>
+                <div className="uk-card-body">
+                    <ul>
+                        {nowBlock.tabs.map((tab, index) => {
+                            return <Tab tab={tab} deleteClick={() => deleteClick(index)}/>;
+                        })}
+                    </ul>
                 </div>
             </div>
-            <div className="uk-card-body">
-                <ul>
-                    {block.block.tabs.map((tab) => {
-                        return TabDom(tab);
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
-};
-
-export const BlockDom = (block: model.BlockAndKey) => <Block block={block.block} IDkey={block.IDkey}/>;
-
-const Tab: React.FC<model.Tab> = (tab) => {
-        const [nowTab, setNowTab] = useState(tab);
-        const [isDisplayed, setIsDisplayed] = useState(true);
-
-        const deleteTab = () => {
-            setIsDisplayed(false)
-        }
-
-        if (isDisplayed) {
-            const domain = util.getDomain(nowTab.url);
-            const encode_domain = (domain === "") ? encodeURI(" ") : encodeURI(domain);
-            const encode_url = util.escape_html(tab.url);
-            const encode_title = util.escape_html(tab.title);
-
-
-            return (
-                <li className="tab-root-dom">
-                    <img src={`https://www.google.com/s2/favicons?domain=${encode_domain}`} alt={encode_title}/>
-                    <a href={encode_url} className="tab_link" data-url={encode_url}
-                       data-title={encode_title}>{encode_title}</a>
-                    <span className="uk-link tab_close" uk-icon="icon: close; ratio: 0.9" onClick={deleteTab}/>
-                </li>
-            );
-        } else {
-            return (<div style={display_none}/>);
-        }
+        );
+    } else {
+        return (<div style={display_none}/>);
     }
-;
+};
 
 const display_none = {
     display: "none"
 };
 
 
-export const TabDom = (tab: model.Tab) => <Tab url={tab.url} title={tab.title}/>;
+export const BlockDom = (block: model.BlockAndKey) => <Block block={block.block} IDkey={block.IDkey}/>;
+
+
+interface TabProps {
+    tab: model.Tab,
+    deleteClick: VoidFunction,
+}
+
+const Tab: React.FC<TabProps> = (props) => {
+    const domain = util.getDomain(props.tab.url);
+    const encode_domain = (domain === "") ? encodeURI(" ") : encodeURI(domain);
+    const encode_url = util.escape_html(props.tab.url);
+    const encode_title = util.escape_html(props.tab.title);
+
+
+    return (
+        <li className="tab-root-dom">
+            <img src={`https://www.google.com/s2/favicons?domain=${encode_domain}`} alt={encode_title}/>
+            <a href={encode_url} className="tab_link" data-url={encode_url}
+               data-title={encode_title}>{encode_title}</a>
+            <span className="uk-link tab_close" uk-icon="icon: close; ratio: 0.9"
+                  onClick={props.deleteClick}/>
+        </li>
+    );
+};
 
 export default Block;
