62e97b639b3c400cd44e079f40f9ea8c73e1ef2a
ik-fib <ik11235@gmail.com>
2021-08-08 20:47:15 +0900

    emptyタグを変換

--- src/js/sideBar.tsx
+++ src/js/sideBar.tsx
@@ -7,16 +7,16 @@ const SideBar: React.FC = () => {
                 <ul className="uk-nav-default uk-nav-parent-icon" uk-nav="multiple: true">
                     <li className="uk-nav-header">メニュー</li>
                     <li className="uk-active">
-                        <a href="#" id="all_clear"><span className="uk-margin-small-right" uk-icon="icon: trash"></span>
+                        <a href="#" id="all_clear"><span className="uk-margin-small-right" uk-icon="icon: trash"/>
                             すべてのデータを削除する
                         </a>
                     </li>
                     <li className="uk-parent">
-                        <a href="#"><span className="uk-margin-small-right" uk-icon="icon: pull"></span>export</a>
+                        <a href="#"><span className="uk-margin-small-right" uk-icon="icon: pull"/>export</a>
                         <ul className="uk-nav-sub">
                             <li>
-                                <label htmlFor="export_body"></label><textarea readOnly={true} id="export_body"
-                                                                               rows={4}></textarea>
+                                <label htmlFor="export_body"/>
+                                <textarea readOnly={true} id="export_body" rows={4}/>
                             </li>
                             <li>
                                 <button id="export_link">export実行</button>
@@ -24,9 +24,11 @@ const SideBar: React.FC = () => {
                         </ul>
                     </li>
                     <li className="uk-parent">
-                        <a href="#"><span className="uk-margin-small-right" uk-icon="icon: push"></span>import</a>
+                        <a href="#"><span className="uk-margin-small-right" uk-icon="icon: push"/>import</a>
                         <ul className="uk-nav-sub">
-                            <li><label htmlFor="import_body"></label><textarea id="import_body" rows={4}></textarea>
+                            <li>
+                                <label htmlFor="import_body"/>
+                                <textarea id="import_body" rows={4}/>
                             </li>
                             <li>
                                 <button id="import_link">import実行</button>
