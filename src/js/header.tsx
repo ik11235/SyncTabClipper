f89ae26f743ba14e134613ede7de11871bf925c4
ik-fib <ik11235@gmail.com>
2021-08-09 13:56:55 +0900

    headerをreactコンポーネント化

---
+++ src/js/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    const titleText = chrome.runtime.getManifest().name;
+
+    return (
+        <h1 className='uk-heading-primary uk-heading-divider'>
+            <img src="images/icon.svg" width="32" height="32"/>
+            {titleText}
+        </h1>
+    );
+}
+
+export const HeaderDom = () => <Header />;
+
+export default Header;
--- src/js/tabs.ts
+++ src/js/tabs.ts
@@ -1,18 +1,18 @@
 import UIkit from 'uikit';
 // @ts-ignore
 import Icons from 'uikit/dist/js/uikit-icons';
-import {blockService} from "./blockService";
 import {chromeService} from "./chromeService";
-import {util} from "./util"
 import ReactDOM from "react-dom";
 import {MainDom} from "./components/block";
 import {SideBarDom} from "./sideBar";
+import {HeaderDom} from "./header";
 
 // @ts-ignore
 UIkit.use(Icons);
 
 window.onload = function () {
-  util.replacePageTitle(<HTMLElement>document.getElementsByTagName("h1")[0], chrome.runtime.getManifest().name)
+  const header = document.getElementById('header')!
+  ReactDOM.render(HeaderDom(), header);
 
   const sidebar = document.getElementById('sidebar')!
   ReactDOM.render(SideBarDom(), sidebar);
